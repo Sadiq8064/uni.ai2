0e7e97a326695e08b18cabf9f8502fe4b9cbeabf
make in bvb <70434165+Sadiq8064@users.noreply.github.com>
2025-12-25 03:20:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
     console.log('=================================================');
     console.log('  University Management System API');
